53e623d95f84e1edd237120a07f3b85b620902e9
Henrik <henrik_mathiesen@hotmail.com>
2016-07-08 15:40:30 +0200

    -use reacts official prop files in production

--- gulpfile.js
+++ gulpfile.js
@@ -19,11 +19,20 @@ var isProduction = (argv.prod) ? (true) : (false);
 //
 // Sources
 
-var jsLibSrc = [
+var jsLibSrcDev = [
     './bower_components/jquery/dist/jquery.js',
     './bower_components/react/react.js',
     './bower_components/react/react-dom.js'
 ];
+
+// React complains about minifying the normal react files, we must use their minified versions (its "better"), so we set it up like this
+var jsLibSrcProd = [
+    './bower_components/jquery/dist/jquery.min.js',
+    './bower_components/react/react.min.js',
+    './bower_components/react/react-dom.min.js'
+];
+
+var jsLibSrc = isProduction ? jsLibSrcProd : jsLibSrcDev;
 
 var jsAppSrc = './gulpworkflow/src/**/*.js';
 var bldFolder = './gulpworkflow/bld';
@@ -35,11 +44,10 @@ gulp.task('js-lib', function () {
     return gulp
         .src(jsLibSrc)
         .pipe(gulpif(!isProduction, sourceMaps.init()))
-        
+
         .pipe(concatJs('lib.js'))
-        
-        .pipe(gulpif(isProduction, uglifyJs()))
-        
+        // no need to uglify after concat since they are minified already
+
         .pipe(gulpif(!isProduction, sourceMaps.write()))
         .pipe(gulp.dest(bldFolder));
 });
@@ -48,13 +56,15 @@ gulp.task('js-app', function () {
     return gulp
         .src(jsAppSrc)
         .pipe(gulpif(!isProduction, sourceMaps.init()))
-        
+
         .pipe(babel())
 
         .pipe(concatJs('app.js'))
-        
+
         .pipe(gulpif(isProduction, uglifyJs()))
-        
+
         .pipe(gulpif(!isProduction, sourceMaps.write()))
         .pipe(gulp.dest(bldFolder));
 });
+
+gulp.task('default', ['js-lib', 'js-app'], function () { });
